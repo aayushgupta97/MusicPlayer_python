ec651778111d1ab07d3a9c5de42a63bf2b044a3b
Aayush Gupta <aayushgupta2097@gmail.com>
2019-02-28 15:51:55 +0530

    play/pause functionality

--- app.py
+++ app.py
@@ -3,10 +3,13 @@ import os
 import pygame
 import math 
 import youtube_dl
+from random import randint,shuffle
+import random
+# import sys
 pygame.init()
 
 default = os.listdir('music/')
-print(default)
+# print(default)
 favourite = [] 
 current = [] 
 
@@ -25,8 +28,8 @@ def favourites(item):
     # os.chdir('music/') 
 
 
-print("this is favourite playlist \n")
-print(favourite)
+# print("this is favourite playlist \n")
+# print(favourite)
 
 os.chdir("music/")
 
@@ -38,6 +41,9 @@ def play_playlist(lst):
     print("next - to play the next song in playlist\n")
     print("stop - to stop the playback\n")
     print("fav - to add to favourites\n")
+    print("pause - to add to favourites\n")
+    print("play - to add to favourites\n")
+
     play = True
     while play:
         for song in lst:
@@ -46,15 +52,30 @@ def play_playlist(lst):
             a = pygame.mixer.Sound(song)
             print("length: ",int(a.get_length()//60),"min",math.floor(a.get_length())%60,"seconds")
             pygame.mixer.music.play(0)
-            c = input("what do you want to do:")
-            if c=='next':
-                pygame.mixer.music.stop()
+            
+            playing = True
+            while playing:
+                c = input("what do you want to do:")
+                if c=='next': # exits this while loop
+                    pygame.mixer.music.stop()
+                    playing = False
+                elif c=='stop': # exits all the loops
+                    pygame.mixer.music.stop()
+                    playing = False
+                    # break
+                elif c=='fav':
+                    favourites(song)
+                elif c=='pause':
+                    pygame.mixer.music.pause()
+                elif c=='play':
+                    pygame.mixer.music.unpause()
+                else:
+                    print("Invalid Input! Try (play,pause,stop,fav,next) ")
             if c=='stop':
-                pygame.mixer.music.stop()
                 play = False
                 break
-            if c=='fav':
-                favourites(song)
+
+    main()
 
 
     # main()
@@ -69,13 +90,15 @@ def select_playlist():
     print("2 : Favourite playlist\n")
     lst_id = int(input("Enter the ID of the playlist to play: "))
     show_playlist(all_playlist[lst_id])
-    print("\n1) Play the list\n2)Edit the list\n")
+    print("\n1) Play the list\n2)Edit the list\n3) Shuffle the list")
     choice = int(input("Enter your choice: "))
     if choice == 1 :     
         print(f"Playing the playlist {all_playlist[lst_id]} ")
         play_playlist(all_playlist[lst_id])
     elif choice == 2 : 
         addto_playlist(all_playlist[lst_id])
+    elif choice == 3 : 
+        shuffle_playlist(all_playlist[lst_id])
 
 #Shows all the songs in the passed playlist
 def show_playlist(lst):
@@ -154,15 +177,21 @@ def greeting():
 def end():
     print("goodbye!")   
 def main():
+    print("\n\n\n")
+    print("-"*150 + "\n" + "*" * 150)
     print("Press 1 to select playlist")
     # print("Press 2 to list all songs")
     print("press 2 to select a song")
+    print("press 3 to shuffle all songs")
     print("press 8 to download a song from youtube")
     print("press 9 to exit")
+    print("*"*150 + "\n" + "-" * 150)
+
     my_dict = {
         1:select_playlist,
         # 2:playlist,
         2:songs,
+        3:shuffle,
         8:youtube,
         9:end
     }
@@ -192,7 +221,22 @@ def youtube():
         # default = os.listdir()
         # print(default)
     main()
+# shuffled_playlist = [] 
 
+def shuffle_playlist(lst=default):
+    # shuffling = 'y'
+    # while shuffling=='y' :
+    #     p = randint(0,len(lst)-1)
+    #     pygame.mixer.music.load(lst[p])
+    #     pygame.mixer.music.play(0)
+    #     # main()
+    #     shuffling = input("listen to next song? y/n: ")
+    shuffled_playlist = lst[:]
+    shuffle(shuffled_playlist)
+    # print(shuffled_playlist)
+
+    play_playlist(shuffled_playlist)
+    
 
 greeting()
 # select_playlist()
