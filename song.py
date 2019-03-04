abd62a811988b7d2417caf1fc0ddb387e3470e2b
Aayush Gupta <aayushgupta2097@gmail.com>
2019-03-04 12:56:06 +0530

    adding a separate song module

---
+++ song.py
@@ -0,0 +1,37 @@
+from playlist import Playlist
+import pygame
+import os
+
+default = os.listdir('/home/ttn/Documents/MusicProject/src/music/')
+print(default)
+class Song:
+    def __init__(self, song_id):
+        self.song_id = song_id
+    def play_song(self):
+        """
+        takes the input from the select_song function and plays the song 
+        at the selected id
+        """
+        pygame.mixer.music.load(default[self.song_id])
+        print(default[self.song_id])
+        pygame.mixer.music.play(0)
+        c = input("press y to go back to main menu.")
+        if c == 'y':
+            pygame.mixer.music.stop()
+            print("stopping playback.")
+        else:
+            exit()
+
+def select_song():
+    """Take an integer input for the id and plays the song at the 
+    selected id in the default playlist.
+    """
+    play_list = Playlist()
+    play_list.show_playlist(default)
+    song_id = int(input("What song would you like to play? "))
+    song_id -= 1
+    song_object = Song(song_id)
+    song_object.play_song()
+
+
+
